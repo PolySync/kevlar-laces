823ca02919419e302d63c46bbf17aa897252f7f8
Rusell Mull <rmull@polysync.io>
2018-06-07 11:23:13 -0700

    Adding RAII wrapper for workspace prep/restore

---
+++ src/errors.rs
@@ -0,0 +1,13 @@
+
+error_chain!{
+    foreign_links {
+        Rsl(::git_rsl::errors::Error);
+        Git(::git2::Error);
+        // Serde(::serde_json::Error);
+        // GPGME(::gpgme::Error);
+        IO(::std::io::Error);
+    }
+
+    errors {
+    }
+}
--- src/git-merge-pr.rs
+++ src/git-merge-pr.rs
@@ -1,5 +1,21 @@
+extern crate git2;
+extern crate git_rsl;
 
+#[macro_use]
+extern crate error_chain;
+
+mod workspace;
+mod errors;
+
+
+use workspace::Workspace;
+use git_rsl::{prep_workspace, restore_workspace};
+use git_rsl::utils::git::discover_repo;
+use errors::*;
 
 fn main() {
+    let repo = discover_repo().unwrap(); 
+    let ws = Workspace::new(repo).unwrap();
+
     println!("merge-pr");
 }
